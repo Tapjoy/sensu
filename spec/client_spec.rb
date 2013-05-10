10e878ae909ef836d3e6982ecd98dae918c6ad9a
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2013-05-10 09:43:35 -0400

    Fix a variety of issues with check timeouts / forks: * Fix default check timeouts not being respected * Fix checks that timeout resulting in zombie processes * Fix child process not being killed when parent process is killed * Move timeout code from the sensu client to the check itself using IO.select * Change timeout exit code to 3

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -80,8 +80,7 @@ describe 'Sensu::Client' do
   end
 
   it 'supports timeouts for hanging processes' do
-    lambda { @client.execute_with_ruby_fork("#{File.dirname(__FILE__)}/ruby_fork_test.rb \"arg list\" of things \"^reg ex$\"", 1) }.should raise_error Timeout::Error
-    lambda { @client.execute_with_ruby_fork("#{File.dirname(__FILE__)}/ruby_fork_test.rb \"arg list\" of things \"^reg ex$\"", 3) }.should_not raise_error
+    @client.execute_with_ruby_fork("#{File.dirname(__FILE__)}/ruby_fork_test.rb \"arg list\" of things \"^reg ex$\"", 1).should =~ ["Unexpected error: execution expired\n", 3]
     @client.execute_with_ruby_fork("#{File.dirname(__FILE__)}/ruby_fork_test.rb \"arg list\" of things \"^reg ex$\"", 3).should =~ ["ruby forked process with 4 args [\"arg list\", \"of\", \"things\", \"^reg ex$\"]", 0]
   end
 
