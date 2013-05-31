465a1a1162e299baa6e9d32b4cb9a22887f33878
deanxorr <dean.dieker@tapjoy.com>
2013-05-31 12:37:28 -0400

    added ability to gracefully handle poorly formatted json to the determine_stale_clients method in server

--- lib/sensu/server.rb
+++ lib/sensu/server.rb
@@ -581,27 +581,32 @@ module Sensu
     def determine_stale_clients
       @logger.info('determining stale clients')
       @redis.smembers('clients') do |clients|
-        clients.each do |client_name|
-          @redis.get('client:' + client_name) do |client_json|
-            client = Oj.load(client_json)
-            check = {
-              :name => 'keepalive',
-              :issued => Time.now.to_i,
-              :executed => Time.now.to_i
-            }
-            time_since_last_keepalive = Time.now.to_i - client[:timestamp]
-            case
-            when time_since_last_keepalive >= 180
-              check[:output] = 'No keep-alive sent from client in over 180 seconds'
-              check[:status] = 2
-            when time_since_last_keepalive >= 120
-              check[:output] = 'No keep-alive sent from client in over 120 seconds'
-              check[:status] = 1
-            else
-              check[:output] = 'Keep-alive sent from client less than 120 seconds ago'
-              check[:status] = 0
+          time = Time.now.to_i
+          client_key = "client:#{client_name}"
+          @redis.get(client_key) do |client_json|
+            begin
+              client = Oj.load(client_json.to_s)
+              check = {
+                :name => 'keepalive',
+                :issued => time,
+                :executed => time
+              }
+              time_since_last_keepalive = time - client[:timestamp]
+              case
+              when time_since_last_keepalive >= 180
+                check[:output] = 'No keep-alive sent from client in over 180 seconds'
+                check[:status] = 2
+              when time_since_last_keepalive >= 120
+                check[:output] = 'No keep-alive sent from client in over 120 seconds'
+                check[:status] = 1
+              else
+                check[:output] = 'Keep-alive sent from client less than 120 seconds ago'
+                check[:status] = 0
+              end
+              publish_result(client, check)
+            rescue Oj::ParseError => e
+              @logger.warn("Unable to parse client entry #{client_key.inspect} : #{client_json.inspect} : #{e}")
             end
-            publish_result(client, check)
           end
         end
       end
